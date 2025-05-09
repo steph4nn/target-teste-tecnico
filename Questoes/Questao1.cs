39bfd25dd4db9c5ebec3a4cced7f5a84ad54bd8f
steph4nn <laurostephan@gmail.com>
2025-05-09 10:15:22 -0300

    Adicionar arquivos de projeto.

---
+++ Questoes/Questao1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace target_teste_tecnico
+{
+    public class Questao1
+    {
+        public static void Execute()
+        {
+            int index = 13;
+            int sum = 0, k = 0;
+
+            while (k < index)
+            {
+                k = k+ 1;
+                sum = sum+ k;
+            }
+
+            Console.WriteLine($"O valor final da variável sum é: {sum}");
+        }
+
+    }
+}
