26930ab25061173d654b01b67666f1e2c760a68c
steph4nn <laurostephan@gmail.com>
2025-05-09 11:20:06 -0300

    feat: q4

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ namespace target_teste_tecnico
         static void Main(string[] args)
         {
             Questao1.Execute();
+            Questao2.Execute();
+            Questao3.Execute();
+            Questao4.Execute();
         }
     }
 }
---
+++ Questoes/Questao4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace target_teste_tecnico
+{
+    public class Questao4
+    {
+        public static void Execute()
+        {
+            var revenuePerState = new Dictionary<string, double>
+            {
+                { "SP", 67836.43 },
+                { "RJ", 36678.66 },
+                { "MG", 29229.88 },
+                { "ES", 27165.48 },
+                { "Outros", 19849.53 }
+            };
+
+
+            double totalRevenue = 0;
+            foreach (var valor in revenuePerState.Values)
+            {
+                totalRevenue += valor;
+            }
+
+            Console.WriteLine("percentual de cada estado:");
+            foreach (var state in revenuePerState)
+            {
+                double percentual = (state.Value / totalRevenue) * 100;
+                Console.WriteLine($"{state.Key}: {percentual:F2}%");
+            }
+        }
+
+    }
+}
